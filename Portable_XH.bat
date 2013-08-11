b5549cfad6c8b3b1974e715af1fa86fded84623b
cmb69 <cmbecker69@gmx.de>
2013-08-11 20:36:48 +0000

    added Portable_XH.bat

---
+++ Portable_XH.bat
@@ -0,0 +1,30 @@
+@echo off
+
+rem
+rem Portable_XH
+rem
+rem Starts the webserver and opens the browser pointing to CMSimple_XH.
+rem
+rem Copyright 2013 Christoph M. Becker <http://3-magi.net/>
+rem
+rem Permission is hereby granted, free of charge, to any person obtaining a copy
+rem of this software and associated documentation files (the "Software"), to deal
+rem in the Software without restriction, including without limitation the rights
+rem to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+rem copies of the Software, and to permit persons to whom the Software is
+rem furnished to do so, subject to the following conditions:
+rem
+rem The above copyright notice and this permission notice shall be included in
+rem all copies or substantial portions of the Software.
+rem
+rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+rem AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+rem LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+rem OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+rem THE SOFTWARE.
+rem
+
+start "Portable_XH" /min php\php -S localhost:8888 -t www
+start http://localhost:8888/
