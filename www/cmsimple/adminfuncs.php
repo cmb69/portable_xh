5081b7f3d70f0c976cd40ae6eebbd1678e99c38b
cmb69 <cmbecker69@gmx.de>
2015-01-31 14:47:51 +0000

    fixed XH_isAccessProtected() for the single threaded webserver

--- www/cmsimple/adminfuncs.php
+++ www/cmsimple/adminfuncs.php
@@ -179,6 +179,10 @@ function XH_absoluteUrlPath($path)
  */
 function XH_isAccessProtected($path)
 {
+    // Portable_XH can't do this check, but as it is not accessible from
+    // outside, we simply report that everything is okay
+    return true;
+
     global $sn;
 
     $host = $_SERVER['HTTP_HOST'];
