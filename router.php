5e84e6bc4d61c28d330d324ff8c6eef23cae6d01
Christoph M. Becker <cmbecker69@gmx.de>
2016-11-29 15:12:10 +0100

    Bump copyright year and removed SVN Id keywords

--- router.php
+++ router.php
@@ -5,9 +5,7 @@
  *
  * Simple router script for PHP's built-in webserver.
  *
- * Copyright 2014-2015 Christoph M. Becker <http://3-magi.net/>
- *
- * $Id$
+ * Copyright 2014-2016 Christoph M. Becker <http://3-magi.net/>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
